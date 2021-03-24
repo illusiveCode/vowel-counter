00b5630761e045565753c204f992e091b12cf4cf
Muhammad Raza <mh.raza2906@gmail.com>
2021-03-24 19:50:22 +0000

    Create layout & func

---
+++ main.js
@@ -0,0 +1,14 @@
+let count = document.getElementById("vowelCount");
+
+function countVowel(str) {
+  str = str.toLowerCase();
+  let vowArr = ["a", "e", "i", "o", "u"],
+    counter = 0;
+
+  for (let letter of str) {
+    if (vowArr.includes(letter)) {
+      counter++;
+    }
+  }
+  vowelCount.innerHTML = counter;
+}
